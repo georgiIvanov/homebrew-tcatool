c56e10e1fa5ee4094be0860f81e042a43729bc46
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-10-31 18:00:10 +0200

    Change formula

--- tcatool.rb
+++ tcatool.rb
@@ -5,12 +5,6 @@ class Tcatool < Formula
   sha256 "3220392265c0f8fc5243891b27618d249123b93dd932d797b01c5b72faa11a83"
   license "MIT"
 
-  def install
-    bin.install 'tcatool'
-    bin.install Dir['code_generation']
-    bin.install Dir['models']
-  end
-
   depends_on "click"
 
 end
