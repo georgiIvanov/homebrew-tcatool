a08b645d6a70821f50019cdf3aad4f23636afe5f
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2021-10-31 22:18:42 +0200

    Change formula

--- tcatool.rb
+++ tcatool.rb
@@ -1,14 +1,19 @@
 class Tcatool < Formula
+  include Language::Python::Virtualenv
+
   desc "CLI that helps automate some of the work necessary for using The Composable Architecture"
   homepage "https://github.com/georgiIvanov/tcatool"
-  url "https://github.com/georgiIvanov/tcatool/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3220392265c0f8fc5243891b27618d249123b93dd932d797b01c5b72faa11a83"
+  url "https://github.com/georgiIvanov/tcatool/releases/download/v0.1.1/tcatool-0.1.1.tar.gz"
+  sha256 "64240981c31abdb5d0e17b2f48b4687cffca4bad5054043818c9f0a548352c71"
   license "MIT"
 
-  depends_on "click"
+  depends_on "python"
 
   def install
-    system "install"
+    virtualenv_install_with_resources
   end
 
+  test do
+    system "false"
+  end
 end
